7e342a33d2b53f1b1aaecd2a94d48937b9a824d1
Dennis Walters <pooster@gmail.com>
2019-01-05 04:30:16 -0500

    Tests, docs

--- sekrat-crypter-aes.gemspec
+++ sekrat-crypter-aes.gemspec
@@ -23,7 +23,8 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_dependency 'sekrat', '~> 1.0.0'
-  spec.add_development_dependency "bundler", "~> 1.16"
-  spec.add_development_dependency "rake", "~> 10.0"
-  spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency 'bundler', '~> 1.16'
+  spec.add_development_dependency 'rake', '~> 10.0'
+  spec.add_development_dependency 'rspec', '~> 3.0'
+  spec.add_development_dependency 'simplecov', '~> 0.16'
 end
--- spec/sekrat/crypter/aes_spec.rb
+++ spec/sekrat/crypter/aes_spec.rb
@@ -1,9 +1,61 @@
+require 'securerandom'
+
 RSpec.describe Sekrat::Crypter::Aes do
   it "has a version number" do
     expect(Sekrat::Crypter::Aes::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '.encrypt' do
+    let(:key) {SecureRandom.hex(5)}
+    let(:data) {SecureRandom.hex(32)}
+    let(:encrypt) {described_class.encrypt(key, data)}
+
+    context 'when all goes well' do
+      it 'is a string' do
+        expect(encrypt).to be_a(String)
+      end
+
+      it 'is reversiable with the same key' do
+        expect(described_class.decrypt(key, encrypt)).to eql(data)
+      end
+    end
+
+    context 'when problems occur' do
+      before(:each) do
+        allow_any_instance_of(OpenSSL::Cipher).
+          to receive(:final).
+          and_raise(StandardError)
+      end
+
+      it 'raises an error' do
+        expect {encrypt}.to raise_error(Sekrat::EncryptFailure)
+      end
+    end
   end
+
+  describe '.decrypt' do
+    let(:key) {SecureRandom.hex(5)}
+    let(:data) {SecureRandom.hex(32)}
+    let!(:encrypted) {described_class.encrypt(key, data)}
+    let(:decrypt) {described_class.decrypt(key, encrypted)}
+
+    context 'when all goes well' do
+      it 'is the original data' do
+        expect(decrypt).to eql(data)
+      end
+    end
+
+    context 'when problems occur' do
+      before(:each) do
+        allow_any_instance_of(OpenSSL::Cipher).
+          to receive(:final).
+          and_raise(StandardError)
+      end
+
+      it 'raises an error' do
+        expect {decrypt}.to raise_error(Sekrat::DecryptFailure)
+      end
+    end
+  end
+
 end
