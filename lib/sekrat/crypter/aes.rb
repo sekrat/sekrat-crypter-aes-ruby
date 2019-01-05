1f4443b37afcdf6d525913611e206aa2297ce014
Dennis Walters <pooster@gmail.com>
2019-01-05 04:07:24 -0500

    Initial commit

---
+++ lib/sekrat/crypter/aes.rb
@@ -0,0 +1,9 @@
+require "sekrat/crypter/aes/version"
+
+module Sekrat
+  module Crypter
+    module Aes
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/sekrat/crypter/aes/version.rb
@@ -0,0 +1,7 @@
+module Sekrat
+  module Crypter
+    module Aes
+      VERSION = "0.1.0"
+    end
+  end
+end
